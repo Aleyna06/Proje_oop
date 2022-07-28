2cb2d7f940ca959d12ada8b4683e64202b6cd3eb
Aleyna06 <celikaleyna71@gmail.com>
2022-07-28 15:52:33 +0300

    Bölüm1 Sonu

--- Proje_oop/Controllers/DefaultController.cs
+++ Proje_oop/Controllers/DefaultController.cs
@@ -9,9 +9,9 @@ namespace Proje_oop.Controllers
     public class DefaultController : Controller
     {
         //void İslemler()
-       // {
+        // {
         //}
-        void messages ()
+        void messages()
         {
             ViewBag.m1 = "Merhaba bu bir Core projesi";
             ViewBag.m2 = "Aleyna ÇELİK";
@@ -24,15 +24,62 @@ namespace Proje_oop.Controllers
             int sonuc = s1 + s2;
             return sonuc;
         }
+        int cevre()
+        {
+            int kisakenar = 20;
+            int uzunkenar = 50;
+            int sonuc = (2 * kisakenar) + (2 * uzunkenar);
+            return sonuc;
+        }
+
+        int faktorıyel()
+        {
+            int sayi = 5;
+            int fakt = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                fakt = fakt * i;
+            }
+            return fakt;
+        }
+        string cumle()
+        {
+            string c = "Aleyna ÇELİK BİLGİSAYAR MÜHENDİSLİĞİ";
+            return c;
+        }
+        void MesajListesi(string p)
+        {
+            ViewBag.v = p;
+        }
+        void Kullanıcı(string username)
+        {
+            ViewBag.k = username;
+        }
+        int Topla(int s1, int s2)
+        {
+            int sonuc = s1 + s2;
+            return sonuc;
+        }
         public IActionResult Index()
         {
             messages();
+            MesajListesi("parametre ismi");
+            Kullanıcı("Aleyna");
+            ViewBag.y = Topla(20,35);
             return View();
         }
         public IActionResult Urunler()
         {
             messages();
             ViewBag.t = topla();
+            ViewBag.C = cevre();
+            ViewBag.f = faktorıyel();
+            return View();
+        }
+        public IActionResult Musteriler()
+        {
+            ViewBag.h = cumle();
+            Kullanıcı("Aleyna12");
             return View();
         }
     }
